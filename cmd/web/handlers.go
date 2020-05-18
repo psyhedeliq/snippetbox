d8d35975087164aa94dd74441240ff6217025ba5
Cornel <filipcornelcristian@gmail.com>
2020-05-18 13:53:35 +0300

    move error handling into helper methods and revert the deliberate error

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -10,7 +10,7 @@ import (
 // Change the signature of the home handler so it is defined as a method against *application.
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
-		http.NotFound(w, r)
+		app.notFound(w) // use the notFound() helper
 		return
 	}
 
@@ -22,17 +22,13 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		// Because the home handler function is now a method against application it can access its fields, including the error logger. We'll write the log message to this instead of the standard logger.
-		app.errorLog.Println(err.Error())
-		http.Error(w, "Internal Server Error", 500)
+		app.serverError(w, err) // use the serverError() helper
 		return
 	}
 
 	err = ts.Execute(w, nil)
 	if err != nil {
-		// Also update the code here to use the error logger from the application struct.
-		app.errorLog.Println(err.Error())
-		http.Error(w, "Internal Server Error", 500)
+		app.serverError(w, err) // use the serverError() helper
 	}
 }
 
@@ -42,7 +38,7 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 	// Extract the value of the id parameter from the query string and try to convert it to an integer using the strconv.Atoi() function. If it can't be converted to an integer, or the value is less than 1, we return a 404 page not found response.
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
-		http.NotFound(w, r)
+		app.notFound(w) // use the notFound() helper
 		return
 	}
 	// Use the fmt.Fprintf() function to interpolate the id value with our response and write it to the http.ResponseWriter.
@@ -55,7 +51,7 @@ func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 	// Use r.Method to check whether the request is using POST or not. Note that http.MethodPost is a constant equal to the string "POST".
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
-		http.Error(w, "Method Not Allowed", 405)
+		app.clientError(w, http.StatusMethodNotAllowed) // use the clientError() helper
 		return
 	}
 	w.Write([]byte("Create a new snippet..."))
