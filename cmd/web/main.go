de18f600e868050963099e5efc3021e99f7c3219
Cornel <filipcornelcristian@gmail.com>
2020-05-18 08:38:18 +0300

    make the  network address for our server configurable at runtime

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,56 +1,28 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
-	"path/filepath"
 )
 
 func main() {
-	// Use the http.NewServeMux() function to initialize a new servemux, then register the home function as the handler for the "/" URL pattern.
+	// Define a new command-line flag with the name 'addr', a default value of ":4000" and some short help text explaining what the flag controls. The value of the flag will be stored in the addr variable at runtime.
+	addr := flag.String("addr", ":4000", "HTTP network address")
+
+	// Importantly, we use the flag.Parse() function to parse the command-line flag. This reads in the command-line flag value and assigns it to the addr variable. You need to call this *before* you use the addr variable otherwise it will always contain the default value of ":4000". If any errors are encountered during parsing the application will be terminated.
+	flag.Parse()
+
 	mux := http.NewServeMux()
-
 	mux.HandleFunc("/", home)
-	// mux.HandleFunc("/snippet", showSnippet)
-	// mux.HandleFunc("/snippet/create", createSnippet)
-	fileServer := http.FileServer(neuteredFileSystem{http.Dir("./static")})
-	mux.Handle("./static", http.NotFoundHandler())
-	mux.Handle("./static/", http.StripPrefix("./static", fileServer))
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
 
-	// Create a file server which serves files out of the "./ui/static" directory. Note that the path given to the http.Dir function is relative to the project directory root.
-	// fileServer := http.FileServer(http.Dir("./ui/static"))
-
-	// Use the mux.Handle() function to register the file server as the handler for all URL paths that start with "/static/". For matching paths, we strip the "/static" prefix before the request reaches the file server.
-	// mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	fileServer := http.FileServer(http.Dir("./ui/static"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 	// Use the http.ListenAndServe() function to start a new web server. We pass in two parameters: the TCP network address to listen on (in this case ":4000") and the servemux we just created. If http.ListenAndServe() returns an error we use the log.Fatal() function to log the error message and exit. Note that any error returned by http.ListenAndServe() is always non-nil.
-	log.Println("Starting server in :4000")
-	err := http.ListenAndServe(":4000", mux)
+	log.Println("Starting server on %s", *addr)
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
-}
-
-type neuteredFileSystem struct {
-	fs http.FileSystem
-}
-
-func (nfs neuteredFileSystem) Open(path string) (http.File, error) {
-	f, err := nfs.fs.Open(path)
-	if err != nil {
-		return nil, err
-	}
-
-	s, err := f.Stat()
-	if s.IsDir() {
-		index := filepath.Join(path, "index.html")
-		if _, err := nfs.fs.Open(index); err != nil {
-			closeErr := f.Close()
-			if closeErr != nil {
-				return nil, closeErr
-			}
-
-			return nil, err
-		}
-	}
-
-	return f, nil
 }
