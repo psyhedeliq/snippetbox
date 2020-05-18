4d5cc45cf006ff5482d3b530dbdcd86f042b8e49
Cornel <filipcornelcristian@gmail.com>
2020-05-18 11:46:41 +0300

    make errorLog logger available to the home function from main()

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,13 +3,12 @@ package main
 import (
 	"fmt"
 	"html/template"
-	"log"
 	"net/http"
 	"strconv"
 )
 
-// Define a home handler function which writes a byte slice containing "Hello from Snippetbox" as the response body.
-func home(w http.ResponseWriter, r *http.Request) {
+// Change the signature of the home handler so it is defined as a method against *application.
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
@@ -23,20 +22,23 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		log.Println(err.Error())
+		// Because the home handler function is now a method against application it can access its fields, including the error logger. We'll write the log message to this instead of the standard logger.
+		app.errorLog.Println(err.Error())
 		http.Error(w, "Internal Server Error", 500)
 		return
 	}
 
 	err = ts.Execute(w, nil)
 	if err != nil {
-		log.Println(err.Error())
+		// Also update the code here to use the error logger from the application struct.
+		app.errorLog.Println(err.Error())
 		http.Error(w, "Internal Server Error", 500)
 	}
 }
 
 // Add a showSnippet handler function
-func showSnippet(w http.ResponseWriter, r *http.Request) {
+// Change the signature of the showSnippet handler so it is defined as a method against *application.
+func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 	// Extract the value of the id parameter from the query string and try to convert it to an integer using the strconv.Atoi() function. If it can't be converted to an integer, or the value is less than 1, we return a 404 page not found response.
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
@@ -48,7 +50,8 @@ func showSnippet(w http.ResponseWriter, r *http.Request) {
 }
 
 // Add a createSnippet handler function
-func createSnippet(w http.ResponseWriter, r *http.Request) {
+// Change the signature of the createSnippet handler so it is defined as a method against *application.
+func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 	// Use r.Method to check whether the request is using POST or not. Note that http.MethodPost is a constant equal to the string "POST".
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,6 +7,12 @@ import (
 	"os"
 )
 
+// Define an application struct to hold the application-wide dependencies for the web application. For now we'll only include fields for the two custom loggers, but we'll add more to it as the build progresses.
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+}
+
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
@@ -17,10 +23,17 @@ func main() {
 	// Create a logger for writing error messages in the same way, but use stderr as the destination and use the log.Lshortfile flag to include the relevant file name and line number.
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
+	// Initialize a new instance of application containing the dependencies.
+	app := &application{
+		errorLog: errorLog,
+		infoLog:  infoLog,
+	}
+
+	// Swap the route declarations to use the application struct's methods as the // handler functions.
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet", showSnippet)
-	mux.HandleFunc("/snippet/create", createSnippet)
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/snippet", app.showSnippet)
+	mux.HandleFunc("/snippet/create", app.createSnippet)
 
 	fileServer := http.FileServer(http.Dir("./ui/static"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
